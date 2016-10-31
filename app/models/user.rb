06a620b55a91830f94cfea00d8c741013459e96d
Nayan Deshmukh <ndesh@nayan.deshmukh>
2016-10-31 23:33:45 +0530

    Add validation for user model
    Signed-off-by: Nayan Deshmukh <ndesh@nayan.deshmukh>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :name, :presence => true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates :roll, :presence => true
+    validates :phone, :presence => true, :length => { :equal => 10 }
+    validates :dob, :presence => true
+    validates :address, :presence => true
 end
