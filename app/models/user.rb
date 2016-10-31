896e9914f68eaa6f71ed1574ea8ecb78a20734ba
Nayan Deshmukh <ndesh@nayan.deshmukh>
2016-11-01 00:01:53 +0530

    Replace :equal with :is in validation code
    Signed-off-by: Nayan Deshmukh <ndesh@nayan.deshmukh>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     validates :name, :presence => true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     validates :roll, :presence => true
-    validates :phone, :presence => true, :length => { :equal => 10 }
+    validates :phone, :presence => true, :length => { :is => 10 }
     validates :dob, :presence => true
     validates :address, :presence => true
 
