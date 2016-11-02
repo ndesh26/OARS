4b2fc12084a4cdabf106190621ab08413b968dbe
Abhinav Garg <abhigarg@iitk.ac.in>
2016-11-02 11:12:36 +0530

    validation for email uniqueness added
    Signed-off-by: Abhinav Garg <abhigarg@iitk.ac.in>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     validates :name, :presence => true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates :email,:presence =>true, :uniqueness => true
     validates :roll, :presence => true
     validates :phone, :presence => true, :length => { :is => 10 }
     validates :dob, :presence => true
