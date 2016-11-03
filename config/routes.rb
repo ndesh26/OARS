b3ca161044b745ff0fe59731f598fe663953f9f2
Nayan Deshmukh <ndesh@iitk.ac.in>
2016-11-03 20:35:21 +0530

    routes: add route for course request
    Signed-off-by: Nayan Deshmukh <ndesh@iitk.ac.in>

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     get 'sessions/new'   
     resources :users do
         resources :course_completeds
+        member do
+            get :request_course
+        end
     end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'users#index'
