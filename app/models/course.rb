a5a2e8893cfc5fde22470b764f5c1f077ce64ea1
Nayan Deshmukh <ndesh@iitk.ac.in>
2016-11-03 20:12:37 +0530

    model/course: add a list of user which have requested for this course
    use has_many and through to implement this from the user_requests array
    
    Signed-off-by: Nayan Deshmukh <ndesh@iitk.ac.in>

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ApplicationRecord
     has_many :user_requests, class_name: "Request", foreign_key: "course_id", dependent: :destroy
+    has_many :users, through: :user_requests
 
 end
