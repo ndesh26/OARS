20caac0622ce995d0d93434d6179dbdf10225016
Abhinav Garg <abhigarg@iitk.ac.in>
2016-11-06 17:37:53 +0530

    Added authorization for viewing and requesting the courses Also denied other users to even see the info of other as in oars
    Signed-off-by: Abhinav Garg <abhigarg@iitk.ac.in>

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,6 @@
 class RequestsController < ApplicationController
+    before_action :logged_in_user
+    before_action :correct_user
     def create
         user = User.find(params[:user_id])
         course = Course.find(params[:course_id])
@@ -13,4 +15,19 @@ class RequestsController < ApplicationController
         user.delete_request(course)
         redirect_to request_course_user_path(user)
     end
+
+    private
+    def logged_in_user
+        unless logged_in?
+            store_location
+            redirect_to login_url
+        end
+    end
+
+    def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless current_user?(@user)
+    end
+
+
 end
