cec3ebcc8c61321f993357018e521859c2344c98
Abhinav Garg <abhigarg@iitk.ac.in>
2016-11-09 00:51:11 +0530

    Revert "models: Linked Each Course to an Instructor"
    This reverts commit 50378f807dd6a20d239f389842c87432fb3d524b.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
     has_many :user_requests, class_name: "Request", foreign_key: "course_id", dependent: :destroy
     has_many :users, through: :user_requests
-    belongs_to :instructor
+
     def accepted?(user)
         return user_requests.find_by(user_id: user.id).status == "Accepted"
     end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -2,7 +2,7 @@ class Instructor < ApplicationRecord
     validates :name, :presence => true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     validates :email,:presence =>true, :uniqueness => true
-    has_one :course     
+ 
     validates :password, :presence => true, :length => { :in => 6..20 }
     has_secure_password
  end
