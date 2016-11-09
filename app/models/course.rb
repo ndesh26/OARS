95797f29d5bdb73b097f8bc638ed5c46f6c7d918
Abhinav Garg <abhigarg@iitk.ac.in>
2016-11-09 09:39:55 +0530

    Added functionality of an instructor to take multiple courses
    Signed-off-by: Abhinav Garg <abhigarg@iitk.ac.in>

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ApplicationRecord
     has_many :user_requests, class_name: "Request", foreign_key: "course_id", dependent: :destroy
     has_many :users, through: :user_requests
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    validates :email,:presence =>true, :uniqueness => true
+    #validates :email,:presence =>true, :uniqueness => true
     def accepted?(user)
         return user_requests.find_by(user_id: user.id).status == "Accepted"
     end
