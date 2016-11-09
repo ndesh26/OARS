9f2fa6a7d809d858a17beadb0e52c96c7276795f
Abhinav Garg <abhigarg@iitk.ac.in>
2016-11-09 10:41:55 +0530

    Added validation to allow only existing instructor to take courses
    Signed-off-by: Abhinav Garg <abhigarg@iitk.ac.in>

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,11 @@ class Course < ApplicationRecord
     has_many :users, through: :user_requests
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     #validates :email,:presence =>true, :uniqueness => true
+    validate :valid_instructor
+
+    def valid_instructor
+        self.errors.add(:email, 'instructor does not exist') if !(Instructor.where(email: self.email).exists?)
+    end
     def accepted?(user)
         return user_requests.find_by(user_id: user.id).status == "Accepted"
     end
